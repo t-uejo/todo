55c8b1fe69be00d42813272fd5d9b8fab0991014
uejo <t.uejo@ntt-ipnet.co.jp>
2024-11-20 13:40:07 +0900

    initial commit

---
+++ src/main/java/com/example/todo/config/app/TodoDomainConfig.java
@@ -0,0 +1,50 @@
+package com.example.todo.config.app;
+
+import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.terasoluna.gfw.common.exception.ExceptionLogger;
+import org.terasoluna.gfw.common.exception.ResultMessagesLoggingInterceptor;
+
+/**
+ * Bean definitions for domain layer.
+ */
+@Configuration
+@ComponentScan(basePackages = { "com.example.todo.domain" })
+@Import({ TodoInfraConfig.class,
+    TodoCodeListConfig.class })
+public class TodoDomainConfig {
+
+    /**
+     * Configure messages logging AOP.
+     * @param exceptionLogger Bean defined by ApplicationContext#exceptionLogger
+     * @see com.example.todo.config.app.ApplicationContext#exceptionLogger()
+     * @return Bean of configured {@link ResultMessagesLoggingInterceptor}
+     */
+    @Bean("resultMessagesLoggingInterceptor")
+    public ResultMessagesLoggingInterceptor resultMessagesLoggingInterceptor(
+            ExceptionLogger exceptionLogger) {
+        ResultMessagesLoggingInterceptor bean = new ResultMessagesLoggingInterceptor();
+        bean.setExceptionLogger(exceptionLogger);
+        return bean;
+    }
+
+    /**
+     * Configure messages logging AOP advisor.
+     * @param resultMessagesLoggingInterceptor Bean defined by #resultMessagesLoggingInterceptor
+     * @see #resultMessagesLoggingInterceptor(ExceptionLogger)
+     * @return Advisor configured for PointCut
+     */
+    @Bean
+    public Advisor resultMessagesLoggingInterceptorAdvisor(
+            ResultMessagesLoggingInterceptor resultMessagesLoggingInterceptor) {
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression(
+                "@within(org.springframework.stereotype.Service)");
+        return new DefaultPointcutAdvisor(pointcut, resultMessagesLoggingInterceptor);
+    }
+}
---
+++ src/main/java/com/example/todo/config/app/TodoInfraConfig.java
@@ -0,0 +1,11 @@
+package com.example.todo.config.app;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Bean definitions for infrastructure layer.
+ */
+@Configuration
+public class TodoInfraConfig {
+
+}
