9edb4862086eb20570b8d3b01584862e59061317
uejo <t.uejo@ntt-ipnet.co.jp>
2024-12-10 15:18:28 +0900

    feature：Thymeleafを導入

--- src/main/java/com/example/todo/controller/IndexController.java
+++ src/main/java/com/example/todo/controller/IndexController.java
@@ -2,13 +2,12 @@ package com.example.todo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller("/")
 public class IndexController {
+
     @GetMapping
-    @ResponseBody
     public String index(){
-        return "Hello, world!";
+        return "index";
     }
 }
