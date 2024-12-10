a5c9340a571d9732f9d2e267f3e18b7364a0a5be
uejo <t.uejo@ntt-ipnet.co.jp>
2024-12-10 16:45:20 +0900

    feature：foreachを導入

--- src/main/java/com/example/todo/controller/task/TaskController.java
+++ src/main/java/com/example/todo/controller/task/TaskController.java
@@ -4,13 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class TaskController {
 
     @GetMapping("/tasks")
     public String list(Model model){
-        var taskForm = new TaskForm(1L, "Spring Bootを学ぶ", "TODOアプリケーションを作ってみる", "DONE");
-        model.addAttribute("task", taskForm);
+        var taskForm1 = new TaskForm(1L, "Spring Bootを学ぶ", "TODOアプリケーションを作ってみる", "DONE");
+        var taskForm2 = new TaskForm(2L, "Spring Bootを学ぶ", "TODOアプリケーションを作ってみる", "DONE");
+
+        var taskList = List.of(taskForm1,taskForm2);
+        model.addAttribute("taskList", taskList);
         return "tasks/list";
     }
 }
