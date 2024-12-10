e445cac471bb40f8b0aafdcd037a50870793f591
uejo <t.uejo@ntt-ipnet.co.jp>
2024-12-10 15:59:30 +0900

    feature：一覧ページを作成し、リンクを追加

---
+++ src/main/java/com/example/todo/controller/task/TaskController.java
@@ -0,0 +1,13 @@
+package com.example.todo.controller.task;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TaskController {
+
+    @GetMapping("/tasks")
+    public String list(){
+        return "tasks/list";
+    }
+}
