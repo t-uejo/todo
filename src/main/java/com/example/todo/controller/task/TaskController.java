d234601ca5e918834a9a424c58dbf81858b138ae
uejo <t.uejo@ntt-ipnet.co.jp>
2024-12-12 10:49:05 +0900

    feature：バリデーションがNGだった場合、入力データを保持させる処理

--- src/main/java/com/example/todo/controller/task/TaskController.java
+++ src/main/java/com/example/todo/controller/task/TaskController.java
@@ -4,6 +4,8 @@ import com.example.todo.service.task.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,12 +37,19 @@ public class TaskController {
     }
 
     @GetMapping("/creationForm")
-    public String showCreationForm(){
+    public String showCreationForm(TaskForm form, Model model){
+        if(form == null) {
+            form = new TaskForm(null,null, null);
+        }
+        model.addAttribute("taskForm", form);
         return "tasks/form";
     }
 
     @PostMapping
-    public String create(TaskForm form){
+    public String create(@Validated TaskForm form, BindingResult bindingResult, Model model){
+        if (bindingResult.hasErrors()) {
+            return showCreationForm(form, model);
+        }
         taskService.create(form.toEntity());
         return "redirect:/tasks";
     }
