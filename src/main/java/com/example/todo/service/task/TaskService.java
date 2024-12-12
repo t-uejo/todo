5cf9dc79c25ab2bb774c069f169567a0d2b8d5d6
uejo <t.uejo@ntt-ipnet.co.jp>
2024-12-12 13:40:03 +0900

    feature：タスクがなかった場合に404を投げるビジネス例外を定義

--- src/main/java/com/example/todo/service/task/TaskService.java
+++ src/main/java/com/example/todo/service/task/TaskService.java
@@ -1,11 +1,10 @@
 package com.example.todo.service.task;
 
+import com.example.todo.controller.task.TaskNotFoundException;
 import com.example.todo.repository.task.TaskRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -21,7 +20,7 @@ public class TaskService {
 
     public TaskEntity findById(long taskId) {
         return taskRepository.selectById(taskId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Task not found: id = " + taskId));
+                .orElseThrow(TaskNotFoundException::new);
     }
 
     @Transactional
