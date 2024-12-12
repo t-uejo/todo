fece416f7e967a7514d759673cdf72de6c26f4df
uejo <t.uejo@ntt-ipnet.co.jp>
2024-12-12 10:23:07 +0900

    feature：@RequestMappingを使用、TaskFormにtoEntityを実装

--- src/main/java/com/example/todo/controller/task/TaskForm.java
+++ src/main/java/com/example/todo/controller/task/TaskForm.java
@@ -1,7 +1,20 @@
 package com.example.todo.controller.task;
 
+import com.example.todo.service.task.TaskEntity;
+import com.example.todo.service.task.TaskStatus;
+
 public record TaskForm(
         String summary,
         String description,
         String status
-) {}
+) {
+
+    public TaskEntity toEntity() {
+        return new TaskEntity(
+                null,
+                summary(),
+                description(),
+                TaskStatus.valueOf(status())
+        );
+    }
+}
