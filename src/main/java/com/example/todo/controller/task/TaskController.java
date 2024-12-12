d06d88f541d4278571ebcd831060cd79d64d2c42
uejo <t.uejo@ntt-ipnet.co.jp>
2024-12-12 11:08:59 +0900

    feature：TaskFormの引数に@ModelAttributeを宣言することで処理を省略

--- src/main/java/com/example/todo/controller/task/TaskController.java
+++ src/main/java/com/example/todo/controller/task/TaskController.java
@@ -6,10 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -37,18 +34,14 @@ public class TaskController {
     }
 
     @GetMapping("/creationForm")
-    public String showCreationForm(TaskForm form, Model model){
-        if(form == null) {
-            form = new TaskForm(null,null, null);
-        }
-        model.addAttribute("taskForm", form);
+    public String showCreationForm(@ModelAttribute TaskForm form){
         return "tasks/form";
     }
 
     @PostMapping
-    public String create(@Validated TaskForm form, BindingResult bindingResult, Model model){
+    public String create(@Validated TaskForm form, BindingResult bindingResult){
         if (bindingResult.hasErrors()) {
-            return showCreationForm(form, model);
+            return showCreationForm(form);
         }
         taskService.create(form.toEntity());
         return "redirect:/tasks";
